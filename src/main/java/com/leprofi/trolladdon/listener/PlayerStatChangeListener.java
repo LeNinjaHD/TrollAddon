cfa0260644bcb8328a35abce850382efdedb9fb2
LeNinjaHD <cedric@leprofi.com>
2022-02-20 00:41:28 +0100

    Started working on Arenaa Stat disabling

---
+++ src/main/java/com/leprofi/trolladdon/listener/PlayerStatChangeListener.java
@@ -0,0 +1,12 @@
+package com.leprofi.trolladdon.listener;
+
+import de.marcely.bedwars.api.event.player.PlayerStatChangeEvent;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class PlayerStatChangeListener implements Listener {
+    @EventHandler
+    public void onPlayerStatChange(PlayerStatChangeEvent event) {
+        //TODO
+    }
+}
